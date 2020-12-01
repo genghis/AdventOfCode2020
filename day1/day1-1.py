dcfc7b1466f4fe460def4a546b82ccbf0893fd02
Genghis <genghis.philip@gmail.com>
2020-12-01 15:29:47 -0600

    using lambdas instead of a big function

--- day1/day1-1.py
+++ day1/day1-1.py
@@ -5,12 +5,6 @@ input_list = [int(x) for x in open('input.txt', 'r')]
 
 pairs = set((x,y) for x,y in itertools.combinations(input_list, 2))
 
-def check_2020(pair):
-	if sum(pair) == 2020:
-		return True
-	else:
-		return False
-
-solution = list(filter(check_2020, pairs))[0]
+solution = list(filter(lambda x: sum(x) == 2020, pairs))[0]
 
 print(f"{solution[0]} + {solution[1]} = 2020\n{solution[0]} x {solution[1]} = {solution[0]*solution[1]}")
