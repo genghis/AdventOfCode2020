a626ea11aa1a94ac4fc68fe27d2dbbc410a72031
Genghis <genghis.philip@gmail.com>
2020-12-01 13:29:24 -0600

    fixes day1-2 in the UGLIEST WAY POSSIBLE

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -3,7 +3,7 @@ import itertools
 
 input_list = [int(x) for x in open('input.txt', 'r')]
 
-trios = set((x,y,z) for x,y,z in itertools.product(input_list, input_list, input_list))
+trios = set((sorted((x,y,z))[0],sorted((x,y,z))[1],sorted((x,y,z))[2]) for x,y,z in itertools.product(input_list, input_list, input_list))
 
 for i in trios:
 	if i[0]+i[1]+i[2] == 2020:
